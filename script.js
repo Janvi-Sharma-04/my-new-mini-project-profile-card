6a5754bc561283d09b6f610872b559248e42773a
Janvi-Sharma-04 <janavisharma4074@gmail.com>
2025-03-30 16:15:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+function changeColo1(){
+    var color = document.getElementById("color");
+    color.style.backgroundColor = "orangered";
+
+     var btn = document.getElementsByClassName("btn1")[0];
+     btn.style.backgroundColor = "orangered";
+
+     var btn = document.getElementsByClassName("btn1")[1];
+     btn.style.backgroundColor = "orangered";
+}
+
+function changeColo2(){
+    var color = document.getElementById("color");
+    color.style.backgroundColor = "green";
+
+     var btn = document.getElementsByClassName("btn1")[0];
+     btn.style.backgroundColor = "green";
+
+     var btn = document.getElementsByClassName("btn1")[1];
+     btn.style.backgroundColor = "green";
+}
+
+function changeColo3(){
+    var color = document.getElementById("color");
+    color.style.backgroundColor = "purple";
+
+     var btn = document.getElementsByClassName("btn1")[0];
+     btn.style.backgroundColor = "purple";
+
+     var btn = document.getElementsByClassName("btn1")[1];
+     btn.style.backgroundColor = "purple";
+}
+
+function changeColo4(){
+    var color = document.getElementById("color");
+    color.style.backgroundColor = "brown";
+
+     var btn = document.getElementsByClassName("btn1")[0];
+     btn.style.backgroundColor = "brown";
+
+     var btn = document.getElementsByClassName("btn1")[1];
+     btn.style.backgroundColor = "brown";
+}
+
+function changeColo5(){
+    var color = document.getElementById("color");
+    color.style.backgroundColor = "blue";
+
+     var btn = document.getElementsByClassName("btn1")[0];
+     btn.style.backgroundColor = "blue";
+
+     var btn = document.getElementsByClassName("btn1")[1];
+     btn.style.backgroundColor = "blue";
+}
